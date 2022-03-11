116ba238d555b5055f22d35740a02eb9a4670519
Your Name <you@example.com>
2022-03-11 18:22:25 +0700

    random

--- app/src/main/java/com/example/appcalculator18022022/MainActivity.java
+++ app/src/main/java/com/example/appcalculator18022022/MainActivity.java
@@ -21,5 +21,17 @@ public class MainActivity extends AppCompatActivity {
         //3 : Làm tròn lên
 //        double number3 = Math.ceil(1.001);
 //        Log.d("BBB",number3 + "");
+        //4 : Trị tuyệt đối
+//        int number4 = Math.abs(-1);
+//        Log.d("BBB",number4 + "");
+        //5 : Tìm số lớn nhất trong 2 số
+//        int number5 = Math.max(3,4);
+//        Log.d("BBB",number5 + "");
+        //6 : Lấy căn bậc 2
+//        double number6 = Math.sqrt(9);
+//        Log.d("BBB",number6 + "");
+        //7 : Random
+        double number7 = Math.random();
+        Log.d("BBB",number7 + "");
     }
 }
